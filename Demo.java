e26335fdd35edd546a2d210abacf486c6f3346e1
YogeshReferee <yogiyogesh.s.p@gmail.com>
2023-07-21 15:29:09 +0530

    update

--- Demo.java
+++ Demo.java
@@ -3,12 +3,12 @@ package Exception;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("welcome");
-		int num1 = 10;
-		int num2 = 0;
-		System.out.println(num1);
-		System.out.println(num2);
+		int n1 = 10;
+		int n2 = 0;
+		System.out.println(n1);
+		System.out.println(n2);
 		try {
-			System.out.println(num1 / num2);
+			System.out.println(n1 / n2);
 		} catch (Exception e) {
 			// System.out.println("Values not divide by zero ");
 			// System.out.println(e);
